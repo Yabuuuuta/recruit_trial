40fa0f17c08b57a1af54fe4dcfa92fc66d098683
Yabuuuuta <yabuta@lineaworks.jp>
2022-12-23 18:09:43 +0900

    全体調整完了

--- src/js/index.js
+++ src/js/index.js
@@ -43,175 +43,173 @@ import EVENTS from '~/constants/event-names';
 })();
 
 
+$(function () {
+  // header ===============================================
 
+  const header = () => {
+    const $bgrBtn = $('.p-header__bgr');
+    const $menu = $('.p-menu');
+    const $menuClose = $('.js-menu-close');
 
-$(function() {
-
-// header fix ===================================
-  let keyHeight = $('.p-key').height();
-
-  function FixedAnime(){
-    let scrollHeight = $(window).scrollTop();
-
-    if (scrollHeight >= keyHeight){
-      $('body').addClass('is-in');
-    } else {
-      $('body').removeClass('is-in');
-    }
-  };
-
-  $(window).scroll(function() {
-    FixedAnime();
-  });
-
-// burger btn ===================================
-  const bgrBtn = $('.p-header__bgr');
-
-  // hover----
-  bgrBtn.hover(
-    function(){
-      $('.p-header__bgr-title').fadeOut();
+    // ハンバーガーメニューのhover Action
+    $bgrBtn.on('mouseenter', () => {
+      $bgrBtn.find('.p-header__bgr-title').fadeOut();
       $('.p-header__bgr span').css('transform', 'translate(-50%, -1rem)');
-    },
-    function(){
-      $('.p-header__bgr-title').fadeIn();
+    });
+    $bgrBtn.on('mouseleave', () => {
+      $bgrBtn.find('.p-header__bgr-title').fadeIn();
       $('.p-header__bgr span').css('transform', 'translate(-50%, 0)');
+    });
+
+    // menuの表示
+    $bgrBtn.on('click', () => {
+      const disabledScroll = (event) => {
+        event.preventDefault();
+      };
+      $menu.fadeIn(200);
+      document.addEventListener('touchmove', disabledScroll);
+      document.addEventListener('mousewheel', disabledScroll);
+    });
+
+    $menuClose.on('click', () => {
+      $menu.fadeOut(200);
+    });
+
+    // key visualを超えたらfixedで固定
+    const $keyHeight = $('.p-key').height();
+
+    function FixedAnime() {
+      const scrollHeight = $(window).scrollTop();
+
+      if (scrollHeight >= $keyHeight) {
+        $('body').addClass('is-in');
+      } else {
+        $('body').removeClass('is-in');
+      }
     }
-  );
 
-  // menu----
-  const menu = $('.p-menu');
-  const menuClose = $('.js-menu-close');
-
-  function disabledScroll(event) {
-    event.preventDefault();
+    $(window).scroll(function () {
+      FixedAnime();
+    });
   };
+  header();
 
-  bgrBtn.on('click', function(){
-    menu.fadeIn(200);
-    document.addEventListener('touchmove', disabledScroll);
-    document.addEventListener('mousewheel', disabledScroll);
-  });
+  // go pagetop btn ===============================
 
-  menuClose.on('click', function(){
-    menu.fadeOut(200);
-  });
+  const $goTop = $('.js-gotop');
 
-
-
-// header ===============================================
-
-
-
-
-
-
-// go pagetop btn ===============================
-
-const goTop = $('.js-gotop');
-
-  goTop.on('click', function(){
-    $('body, html').animate({
-      scrollTop: 0
-    }, 600);
+  $goTop.on('click', () => {
+    $('body, html').animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
     return false;
   });
-});
 
+  // slide up ===============================
 
-// slide up ===============================
+  const slideUp = () => {
+    const $upElm = $('.slide-up'); // .slide-upクラスを定数に
 
-// $(window).on('load scroll', function() {
-//   const slideUp = $('.slide-up');
-
-//   slideUp.each(function() { 
-//     let target = $(this).offset().top;
-// 		let scroll = $(window).scrollTop();
-// 		let height = $(window).height();
-// 		if (scroll > target - height){
-// 			$(this).addClass('active');
-// 		};
-//   });
-// });
-
-
-const slideUp = () => {
-  const $upElm = $('.slide-up'); //.slide-upクラスを定数に
-
-  //$.each(対象オブジェクト, function(index繰り返し対象 ,val){ 繰り返し処理 }
-  $upElm.each((i,e) => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {  //入ってきたら
-        const { isIntersecting } = entry; // "交わったら"を定数化
-        if (isIntersecting) { //もし交わったら以下の処理を実行
-          $(e).addClass('active'); //クラス名を追加
-          observer.disconnect(); //全ての対象要素について状態変化の監視停止
-        }
+    // $.each(対象オブジェクト, function(index繰り返し対象 ,val){ 繰り返し処理 }
+    $upElm.each((i, e) => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          // 入ってきたら
+          const { isIntersecting } = entry; // "交わったら"を定数化
+          if (isIntersecting) {
+            // もし交わったら以下の処理を実行
+            $(e).addClass('active'); // クラス名を追加
+            observer.disconnect(); // 全ての対象要素について状態変化の監視停止
+          }
+        });
       });
+      observer.observe(e); // 要素を監視される対象要素として追加
     });
-    observer.observe(e); //要素を監視される対象要素として追加
-  });
-};
-slideUp();
+  };
+  slideUp();
 
+  // 同一処理
+  // $(window).on('load scroll', function() {
+  //   const slideUp = $('.slide-up');
+  //   slideUp.each(function() {
+  //     let target = $(this).offset().top;
+  // 		let scroll = $(window).scrollTop();
+  // 		let height = $(window).height();
+  // 		if (scroll > target - height){
+  // 			$(this).addClass('active');
+  // 		};
+  //   });
+  // });
 
-// slide left ===============================
+  // slide left ===============================
+  const slideLeft = () => {
+    const $leftElm = $('.slide-left');
 
-//  $(window).on('load scroll', function() {
-//   const slideLeft = $('.slide-left');
-
-//   slideLeft.each(function() { 
-//     let target = $(this).offset().top;
-// 		let scroll = $(window).scrollTop();
-// 		let height = $(window).height();
-// 		if (scroll > target - height){
-// 			$(this).addClass('active');
-// 		};
-//   });
-// });
-
-const slideLeft = () => {
-  const $leftElm = $('.slide-left');
-
-  $leftElm.each((i,e) => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        const { isIntersecting } = entry;
-        if (isIntersecting) {
-          $(e).addClass('active');
-          observer.disconnect();
-        }
+    $leftElm.each((i, e) => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const { isIntersecting } = entry;
+          if (isIntersecting) {
+            $(e).addClass('active');
+            observer.disconnect();
+          }
+        });
       });
+      observer.observe(e);
     });
-    observer.observe(e);
-  });
-};
-slideLeft();
+  };
+  slideLeft();
 
+  // set view width (except scrollbar) ===============================
 
+  const setVw = function () {
+    const vw = document.documentElement.clientWidth / 100;
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+  };
+  window.addEventListener('DOMContentLoaded', setVw);
+  window.addEventListener('resize', setVw);
 
-  
-// set view width (except scrollbar) ===============================
+  // product ============================================
+  const product = () => {
+    const $modal = $('.p-modal');
+    const $card = $('.c-product-card');
+    $modal.hide();
 
-const setVw = function() {
-  const vw = document.documentElement.clientWidth / 100;
-  document.documentElement.style.setProperty('--vw', `${vw}px`);
-}
-window.addEventListener('DOMContentLoaded', setVw);
-window.addEventListener('resize', setVw);
+    // サムネイル生成
+    const sliderThumbnail = new Swiper('.slider-thumbnail', {
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
 
+    // スライダーの処理
+    const swiper = new Swiper('.swiper', {
+      modules: [Pagination, Navigation],
+      loop: true,
 
+      // ページネーション
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      // 前後の矢印
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // サムネイル
+      thumbs: {
+        swiper: sliderThumbnail,
+      },
+    });
 
-// product　============================================
-const product = () => {
+    // モーダルの開閉
 
-  const $modal = $('.p-modal');
-  const $card = $('.c-product-card');
-  $modal.hide();
-
-  //モーダルの開閉
-
-    //view more アニメーション
+    // view more アニメーション
     const $btnMore = $('.p-product__list-item');
     $btnMore.on('mouseenter', (e) => {
       e.preventDefault();
@@ -222,59 +220,28 @@ const product = () => {
       $(e.currentTarget).removeClass('hov-active');
     });
 
-
-    //カードクリックで開く
+    // カードクリックで開く
     $card.on('click', (e) => {
       const slideNo = $(e.currentTarget).data('slide');
       swiper.slideTo(slideNo, 0);
-  
+
       $modal.fadeIn(300);
+      $('body').css('overflow', 'hidden');
     });
 
-    //closeで閉じる
+    // closeで閉じる
     const $closeElm = $modal.find('[data-close]');
     $closeElm.on('click', (e) => {
       $modal.fadeOut(300);
+      $('body').css('overflow', 'auto');
     });
 
-  //スライダーの処理
-    const swiper = new Swiper(".swiper", {  
-      modules: [Pagination, Navigation],
-      loop: true,
-
-      // ページネーション
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      // 前後の矢印
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      //サムネイル
-      thumbs: {
-        swiper: sliderThumbnail,
-      },
+    // モーダルの外側をクリックしたら閉じる
+    $modal.on('click', (e) => {
+      if (!$(e.target).closest('.p-modal__inner').length) {
+        $modal.fadeOut(300);
+      }
     });
-
-    //サムネイル生成
-    const sliderThumbnail = new Swiper('.slider-thumbnail', {
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-    });
-};
-
-product();
-
-
-
-
-
-  
-
-
-
-
+  };
+  product();
+});
